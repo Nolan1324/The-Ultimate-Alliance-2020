0a87293305a3286a5a7ae466f74fca525fb1cbf7
Nolan <nkuza8@gmail.com>
2019-10-29 18:13:44 -0400

    Puncher, LEDS, HMI Adjustments, Pot Failsafe,

--- app/src/main/java/com/nolankuza/theultimatealliance/main/SlaveFragment.java
+++ app/src/main/java/com/nolankuza/theultimatealliance/main/SlaveFragment.java
@@ -1,5 +1,6 @@
 package com.nolankuza.theultimatealliance.main;
 
+import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
