ba40b05d44012cf8b09f2a898618e026be91789a
Nolan <nkuza8@gmail.com>
2019-10-29 18:13:52 -0400

    Revert "Puncher, LEDS, HMI Adjustments, Pot Failsafe,"
    This reverts commit 0a87293305a3286a5a7ae466f74fca525fb1cbf7.

--- app/src/main/java/com/nolankuza/theultimatealliance/main/SlaveFragment.java
+++ app/src/main/java/com/nolankuza/theultimatealliance/main/SlaveFragment.java
@@ -1,6 +1,5 @@
 package com.nolankuza.theultimatealliance.main;
 
-import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
